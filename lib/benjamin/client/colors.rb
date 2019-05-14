db4eb7f5c6cca43e4487cfbd8327065c8f2bc890
Kasvit <vitalii.kasianchuk@webmil.eu>
2019-05-14 17:03:02 +0300

    add colors methods

--- lib/benjamin/client/colors.rb
+++ lib/benjamin/client/colors.rb
@@ -5,8 +5,24 @@ module Benjamin
     module Colors
       include Benjamin::Request
 
-      def palletes
-        get("/color/GetPalettes")
+      def get_color_detail(number:)
+        get("/color/GetColorDetail", {colorNumber: number})
+      end
+
+      def get_colors_from_numbers(list:, with_colors: false)
+        get("/color/GetColorsFromNumbers", {colorList: list, getDetails: with_colors})
+      end
+
+      def get_palletes(with_colors: false)
+        get("/color/GetPalettes", {colorData: with_colors})
+      end
+
+      def get_palletes_by_category(category:, with_colors: false)
+        get("/color/GetPalettesByCategory", {category: category, colorData: with_colors})
+      end
+
+      def get_palletes_by_code(code:)
+        get("/color/GetPaletteByCode", {code: code})
       end
     end
   end
