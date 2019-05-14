2301a6c067cd466dec90456b1c8a9dd1a56a30fc
Vadym Zakharchenko <vadym.zakharchenko@webmil.com.ua>
2019-05-14 18:14:22 +0300

    product methods

--- lib/benjamin_moore_api/client/products.rb
+++ lib/benjamin_moore_api/client/products.rb
@@ -5,10 +5,25 @@ module BenjaminMooreApi
     module Products
       include BenjaminMooreApi::Request
 
-      def families(category: '', countryCode: 'en-us')
-        get("/product/GetFamilies", category: category, countryCode: countryCode)
+      def products_from_number(product_list:, countryCode: 'en-us')
+        get('/product/GetFamilies', productlist: product_list, countryCode: countryCode)
       end
 
+      def families_by_codes(codes:, countryCode: 'en-us')
+        get('/product/GetFamiliesByCodes', codes: codes, countryCode: countryCode)
+      end
+
+      def product_detail(product_number:, countryCode: 'en-us')
+        get('/product/GetProductDetail', productNumber: product_number, countryCode: countryCode)
+      end
+
+      def families(category: '', countryCode: 'en-us')
+        get('/product/GetProductsFromNumbers', category: category, countryCode: countryCode)
+      end
+
+      def family(code:,countryCode: 'en-us')
+        get('/product/GetFamily', code: code, countryCode: countryCode)
+      end
     end
   end
 end
