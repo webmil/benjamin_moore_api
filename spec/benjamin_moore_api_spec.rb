e3a03d5b86e9ce4c61e214512faceb93de790753
roman.savchak <roman.savchak@webmil.com.ua>
2019-05-14 16:44:39 +0300

    create gem

---
+++ spec/benjamin_moore_api_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe BenjaminMooreApi do
+  it "has a version number" do
+    expect(BenjaminMooreApi::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
