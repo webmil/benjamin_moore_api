471dbe814e4c16b10a414bee77f4258d8b1130dd
roman.savchak <roman.savchak@webmil.com.ua>
2019-05-15 15:38:01 +0300

    rename methods, add json parser

--- examples/test.rb
+++ examples/test.rb
@@ -1,5 +1,5 @@
 require_relative '../lib/benjamin_moore_api'
 
-client = BenjaminMooreApi.client(api_key: 'api_key')
+client = BenjaminMooreApi.client(api_key: 'c654234ebc8261e72b46')
 
 p client.families
