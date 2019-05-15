9f4b673af3e321d9f172b951ff324a6c4ca03d19
roman.savchak <roman.savchak@webmil.com.ua>
2019-05-15 15:38:57 +0300

    typo

--- examples/test.rb
+++ examples/test.rb
@@ -1,5 +1,5 @@
 require_relative '../lib/benjamin_moore_api'
 
-client = BenjaminMooreApi.client(api_key: 'c654234ebc8261e72b46')
+client = BenjaminMooreApi.client(api_key: 'api-key')
 
 p client.families
